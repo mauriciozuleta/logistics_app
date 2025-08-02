1d50e31c2d8172c260c642e3371bc65c296ab699
mauriciozuleta <donreymauri@gmail.com>
2025-08-01 20:46:57 -0500

    Enable Load button to populate route and payload tables dynamically based on route type selection

--- operations/routes.py
+++ operations/routes.py
@@ -1,7 +1,18 @@
+
 from flask import Blueprint, render_template
+from models import Airport, Aircraft
 
 operations = Blueprint("operations", __name__, template_folder="templates")
+
 
 @operations.route('/dashboard')
 def dashboard():
     return render_template('operations/operations_dashboard.html')
+
+@operations.route('/add-route', methods=['GET', 'POST'])
+def add_route():
+    airports = Airport.query.order_by(Airport.city, Airport.iata_code).all()
+    airport_choices = [(a.id, f"{a.city} / {a.iata_code}") for a in airports]
+    aircrafts = Aircraft.query.order_by(Aircraft.short_name).all()
+    aircraft_choices = [(a.id, a.short_name) for a in aircrafts]
+    return render_template('operations/add_route.html', airport_choices=airport_choices, aircraft_choices=aircraft_choices)
